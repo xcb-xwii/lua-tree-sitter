7896dc4a89dcf44bea65f79abed275e0f9c53b73
Wang Rongfei <xrowii@outlook.com>
2023-10-12 02:00:54 +0800

    feat(node): complete implementation of `Node`

--- ldoc/Node.lua
+++ ldoc/Node.lua
@@ -10,8 +10,187 @@
 
 ---
 -- Get the `Tree` the node belongs to.
--- @treturn Node
+-- @treturn Tree
 function Node:tree() end
+
+---
+-- Get the type of the node.
+-- @treturn string
+function Node:type() end
+
+---
+-- Get the start byte of the node.
+-- @treturn integer
+function Node:start_byte() end
+
+---
+-- Get the start `Point` of the node.
+-- @treturn Point
+function Node:start_point() end
+
+---
+-- Get the end byte of the node.
+-- @treturn integer
+function Node:end_byte() end
+
+---
+-- Get the end `Point` of the node.
+-- @treturn Point
+function Node:end_point() end
+
+---
+-- Check if the node is named.
+-- @treturn boolean
+function Node:is_named() end
+
+---
+-- Check if the node is missing.
+-- @treturn boolean
+function Node:is_missing() end
+
+---
+-- Check if the node is extra.
+-- @treturn boolean
+function Node:is_extra() end
+
+---
+-- Check if the node has been edited.
+-- @treturn boolean
+function Node:has_changes() end
+
+---
+-- Check if the node is a syntax error or contains any syntax errors.
+-- @treturn boolean
+function Node:has_error() end
+
+---
+-- Get the immediate parent of the node.
+--
+-- This method may return `nil` if no such node exists.
+-- @treturn Node|nil
+function Node:parent() end
+
+---
+-- Get the child of the node at a given index.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer index
+-- @treturn Node|nil
+function Node:child(index) end
+
+---
+-- Get the field name of the child of the node at a given index.
+--
+-- This method may return `nil` if the node has no field name.
+-- @tparam integer index
+-- @treturn string|nil
+function Node:field_name_for_child(index) end
+
+---
+-- Get the number of children the node has.
+-- @treturn integer
+function Node:child_count() end
+
+---
+-- Get the named child of the node at a given index.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer index
+-- @treturn Node|nil
+function Node:named_child(index) end
+
+---
+-- Get the number of named children the node has.
+-- @treturn integer
+function Node:named_child_count() end
+
+---
+-- Get the child of the node by a given field ID.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer id
+-- @treturn Node|nil
+function Node:child_by_field_id(id) end
+
+---
+-- Get the next sibling of the node.
+--
+-- This method may return `nil` if no such node exists.
+-- @treturn Node|nil
+function Node:next_sibling() end
+
+---
+-- Get the previous sibling of the node.
+--
+-- This method may return `nil` if no such node exists.
+-- @treturn Node|nil
+function Node:prev_sibling() end
+
+---
+-- Get the next named sibling of the node.
+--
+-- This method may return `nil` if no such node exists.
+-- @treturn Node|nil
+function Node:next_named_sibling() end
+
+---
+-- Get the previous named_sibling of the node.
+--
+-- This method may return `nil` if no such node exists.
+-- @treturn Node|nil
+function Node:prev_named_sibling() end
+
+---
+-- Get the first child of the node that extends beyond the given byte offset.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer byte
+-- @treturn Node|nil
+function Node:first_child_for_byte(byte) end
+
+---
+-- Get the first named child of the node that extends beyond the given byte offset.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer byte
+-- @treturn Node|nil
+function Node:first_named_child_for_byte(byte) end
+
+---
+-- Get the smallest node within the node that spans the given range of bytes.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer start_byte
+-- @tparam integer end_byte
+-- @treturn Node|nil
+function Node:descendant_for_byte_range(start_byte, end_byte) end
+
+---
+-- Get the smallest node within the node that spans the given range of `Point`s.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer start_point
+-- @tparam integer end_point
+-- @treturn Node|nil
+function Node:descendant_for_point_range(start_point, end_point) end
+
+---
+-- Get the smallest named node within the node that spans the given range of bytes.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer start_byte
+-- @tparam integer end_byte
+-- @treturn Node|nil
+function Node:named_descendant_for_byte_range(start_byte, end_byte) end
+
+---
+-- Get the smallest named node within the node that spans the given range of `Point`s.
+--
+-- This method may return `nil` if no such node exists.
+-- @tparam integer start_point
+-- @tparam integer end_point
+-- @treturn Node|nil
+function Node:named_descendant_for_point_range(start_point, end_point) end
 
 ---
 -- Metamethods
--- src/node.c
+++ src/node.c
@@ -5,10 +5,18 @@
 
 #include <tree_sitter/api.h>
 
+#include <stdint.h>
+
+#include "point.h"
 #include "tree.h"
 #include "util.h"
 
 void LTS_push_node(lua_State *L, TSNode target, int tree_idx) {
+	if (ts_node_is_null(target)) {
+		lua_pushnil(L);
+		return;
+	}
+
 	LTS_Node *ud = lua_newuserdata(L, sizeof *ud);
 	ud->node = target;
 	lua_pushvalue(L, tree_idx);
@@ -38,6 +46,41 @@ static int LTS_node_tree(lua_State *L) {
 	return 1;
 }
 
+static int LTS_node_type(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	lua_pushstring(L, ts_node_type(self));
+	return 1;
+}
+
+static int LTS_node_start_byte(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	lua_pushinteger(L, ts_node_start_byte(self));
+	return 1;
+}
+
+static int LTS_node_start_point(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	LTS_push_point(L, ts_node_start_point(self));
+	return 1;
+}
+
+static int LTS_node_end_byte(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	lua_pushinteger(L, ts_node_end_byte(self));
+	return 1;
+}
+
+static int LTS_node_end_point(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	LTS_push_point(L, ts_node_end_point(self));
+	return 1;
+}
+
 static int LTS_node_eq(lua_State *L) {
 	TSNode self = *LTS_check_node(L, 1);
 	TSNode other = *LTS_check_node(L, 2);
@@ -55,8 +98,202 @@ static int LTS_node_string(lua_State *L) {
 	return 1;
 }
 
+#define SIMPLE_BOOL_GETTER(prop) \
+	static int LTS_node_##prop(lua_State *L) { \
+		TSNode self = *LTS_check_node(L, 1); \
+		\
+		lua_pushboolean(L, ts_node_##prop(self)); \
+		return 1; \
+	}
+
+SIMPLE_BOOL_GETTER(is_named)
+SIMPLE_BOOL_GETTER(is_missing)
+SIMPLE_BOOL_GETTER(is_extra)
+SIMPLE_BOOL_GETTER(has_changes)
+SIMPLE_BOOL_GETTER(has_error)
+
+#undef SIMPLE_BOOL_GETTER
+
+#define SIMPLE_NODE_GETTER(kind) \
+	static int LTS_node_##kind(lua_State *L) { \
+		LTS_Node self = *LTS_check_lts_node(L, 1); \
+		\
+		lua_settop(L, 0); \
+		lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref); \
+		LTS_push_node(L, ts_node_##kind(self.node), 1); \
+		return 1; \
+	}
+
+SIMPLE_NODE_GETTER(parent)
+
+static int LTS_node_child(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t index = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_child(self.node, index), 1);
+	return 1;
+}
+
+static int LTS_node_field_name_for_child(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+	uint32_t index = luaL_checkinteger(L, 2);
+
+	const char *name = ts_node_field_name_for_child(self, index);
+	if (name) {
+		lua_pushstring(L, name);
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+static int LTS_node_child_count(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	lua_pushinteger(L, ts_node_child_count(self));
+	return 1;
+}
+
+static int LTS_node_named_child(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t index = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_named_child(self.node, index), 1);
+	return 1;
+}
+
+static int LTS_node_named_child_count(lua_State *L) {
+	TSNode self = *LTS_check_node(L, 1);
+
+	lua_pushinteger(L, ts_node_named_child_count(self));
+	return 1;
+}
+
+static int LTS_node_child_by_field_name(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	size_t len;
+	const char *name = luaL_checklstring(L, 2, &len);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_child_by_field_name(self.node, name, len), 1);
+	return 1;
+}
+
+static int LTS_node_child_by_field_id(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	TSFieldId index = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_child_by_field_id(self.node, index), 1);
+	return 1;
+}
+
+SIMPLE_NODE_GETTER(next_sibling)
+SIMPLE_NODE_GETTER(prev_sibling)
+SIMPLE_NODE_GETTER(next_named_sibling)
+SIMPLE_NODE_GETTER(prev_named_sibling)
+
+#undef SIMPLE_NODE_GETTER
+
+static int LTS_node_first_child_for_byte(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t byte = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_first_child_for_byte(self.node, byte), 1);
+	return 1;
+}
+
+static int LTS_node_first_named_child_for_byte(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t byte = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_first_named_child_for_byte(self.node, byte), 1);
+	return 1;
+}
+
+static int LTS_node_descendant_for_byte_range(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t start = luaL_checkinteger(L, 2);
+	uint32_t end = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_descendant_for_byte_range(self.node, start, end), 1);
+	return 1;
+}
+
+static int LTS_node_descendant_for_point_range(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	TSPoint start = *LTS_check_point(L, 2);
+	TSPoint end = *LTS_check_point(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_descendant_for_point_range(self.node, start, end), 1);
+	return 1;
+}
+
+static int LTS_node_named_descendant_for_byte_range(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	uint32_t start = luaL_checkinteger(L, 2);
+	uint32_t end = luaL_checkinteger(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_named_descendant_for_byte_range(self.node, start, end), 1);
+	return 1;
+}
+
+static int LTS_node_named_descendant_for_point_range(lua_State *L) {
+	LTS_Node self = *LTS_check_lts_node(L, 1);
+	TSPoint start = *LTS_check_point(L, 2);
+	TSPoint end = *LTS_check_point(L, 2);
+
+	lua_settop(L, 0);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self.tree_ref);
+	LTS_push_node(L, ts_node_named_descendant_for_point_range(self.node, start, end), 1);
+	return 1;
+}
+
 static const luaL_Reg methods[] = {
 	{ "tree", LTS_node_tree },
+	{ "type", LTS_node_type },
+	{ "start_byte", LTS_node_start_byte },
+	{ "start_point", LTS_node_start_point },
+	{ "end_byte", LTS_node_end_byte },
+	{ "end_point", LTS_node_end_point },
+	{ "is_named", LTS_node_is_named },
+	{ "is_missing", LTS_node_is_missing },
+	{ "is_extra", LTS_node_is_extra },
+	{ "has_changes", LTS_node_has_changes },
+	{ "has_error", LTS_node_has_error },
+	{ "parent", LTS_node_parent },
+	{ "child", LTS_node_child },
+	{ "field_name_for_child", LTS_node_field_name_for_child },
+	{ "child_count", LTS_node_child_count },
+	{ "named_child", LTS_node_named_child },
+	{ "named_child_count", LTS_node_named_child_count },
+	{ "child_by_field_name", LTS_node_child_by_field_name },
+	{ "child_by_field_id", LTS_node_child_by_field_id },
+	{ "next_sibling", LTS_node_next_sibling },
+	{ "prev_sibling", LTS_node_prev_sibling },
+	{ "next_named_sibling", LTS_node_next_named_sibling },
+	{ "prev_named_sibling", LTS_node_prev_named_sibling },
+	{ "first_child_for_byte", LTS_node_first_child_for_byte },
+	{ "descendant_for_byte_range", LTS_node_descendant_for_byte_range },
+	{ "descendant_for_point_range", LTS_node_descendant_for_point_range },
+	{ "named_descendant_for_byte_range", LTS_node_named_descendant_for_byte_range },
+	{ "named_descendant_for_point_range", LTS_node_named_descendant_for_point_range },
 	{ NULL, NULL }
 };
 
